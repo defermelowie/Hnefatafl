055faf2e2b2617391cd0ccc85df3e4799dd12ca0
mika2028 <46018291+mika2028@users.noreply.github.com>
2018-12-20 12:24:39 +0100

    Update Piece.java

--- Hnefatafl/src/hnefatafl/Piece.java
+++ Hnefatafl/src/hnefatafl/Piece.java
@@ -31,4 +31,25 @@ public class Piece {
     public Boolean isAlive() {
         return alive;
     }
-}
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setCoordinates(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    
+    
+    
+    
+    }
+    
+    
+
