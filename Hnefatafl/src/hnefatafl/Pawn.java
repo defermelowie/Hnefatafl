a430bd0101128e5c8d93b8f31daacf249ae31586
lowie2028 <lowie2028@users.noreply.github.com>
2018-12-20 12:43:43 +0100

    Update Pawn.java

--- Hnefatafl/src/hnefatafl/Pawn.java
+++ Hnefatafl/src/hnefatafl/Pawn.java
@@ -12,12 +12,21 @@ package hnefatafl;
 public class Pawn extends Piece {
 
     /**
-     * constructor for pawn
+     * Constructor for pawn
      * @param row Row on the board for the new pawn
      * @param column Column on the board for the new pawn
      * @param color true if White, false if Black
      */
     public Pawn(int row, int column, Boolean color) {
         super(row, column, color);
+    }
+
+    /**
+     * Constructor for pawn
+     * @param coordinates an array with {row, column}
+     * @param color true if White, false if Black
+     */
+    public Pawn(int coordinates[], Boolean color) {
+        super(coordinates[0], coordinates[1], color);
     }
 }
