0ea41f424adfc60c1611f891e31cd6ce37d8ae98
mika2028 <46018291+mika2028@users.noreply.github.com>
2018-12-20 12:11:04 +0100

    update player

--- Hnefatafl/src/hnefatafl/Player.java
+++ Hnefatafl/src/hnefatafl/Player.java
@@ -5,12 +5,35 @@
  */
 package hnefatafl;
 
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+
 /**
  *
  * @author mikaz
  */
 public class Player {
+    private Boolean color;
     private int Pawns;
     private int Kings;
+    private  ArrayList<Piece> pieces=new ArrayList<>();
+/**
+ * 
+ * @param color true if White, false if Black
+ */
+ public Player(Boolean color){
+     this.color = color;
+     if(color == true){
+         for( int i = 0; i < 9; i++){
+             pieces.add(new Pawn(0,0,color));
+         }
+         pieces.add(new King(5,5));
+     }
+     else{
+          for( int i = 0; i <17; i++){
+             pieces.add(new Pawn(0,0,false));
+     }
+         
+ }
     
 }
