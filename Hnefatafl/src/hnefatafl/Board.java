5598f5bb09fd076442961b05e7a73d77f6b8ef4e
lowie2028 <lowie2028@users.noreply.github.com>
2018-12-20 14:25:33 +0100

    Update Board.java

--- Hnefatafl/src/hnefatafl/Board.java
+++ Hnefatafl/src/hnefatafl/Board.java
@@ -5,10 +5,39 @@
  */
 package hnefatafl;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mikaz
  */
 public class Board {
-    
+    private ArrayList<Piece> whitePieces = new ArrayList<>();
+    private ArrayList<Piece> blackPieces = new ArrayList<>();
+
+    private int whiteStartLayout[][] = {
+            {6,4} , {6,5} , {6,6} ,
+            {5,4} , {5,6} ,
+            {4,4} , {4,5} , {4,6} ,
+            {5,5}
+    };
+
+    private int blackStartLayout[][] = {
+            {1,4} , {1,5} , {1,6} , {2,5} ,
+            {4,1} , {5,1} , {6,1} , {5,2} ,
+            {9,4} , {9,5} , {9,6} , {8,5} ,
+            {4,9} , {5,9} , {6,9} , {5,8} ,
+    };
+
+    public Board() {
+
+        for (int i = 0; i < 8; i++){
+            whitePieces.add(new Pawn(whiteStartLayout[i], true));
+        }
+        whitePieces.add(new King(whiteStartLayout[8]));
+
+        for (int i = 0; i < 16; i++){
+            blackPieces.add(new Pawn(blackStartLayout[i], false));
+        }
+    }
 }
