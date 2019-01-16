df5014ebe5e78338265abda5c75717cd96d52bc6
mika2028 <46018291+mika2028@users.noreply.github.com>
2019-01-16 10:55:07 +0100

    javadoc blackplayer

--- Hnefatafl/src/hnefatafl/BlackPlayer.java
+++ Hnefatafl/src/hnefatafl/BlackPlayer.java
@@ -1,14 +1,26 @@
 package hnefatafl;
 
 import java.util.Iterator;
-
+/**
+ * Class for the black player
+ *
+ * @author Stef, Mika, Lowie
+ */
 public class BlackPlayer extends Player {
 
+     /**
+     * Constructor for BlackPlayer
+     */
     public BlackPlayer() {
         super(Color.BLACK);
     }
 
     //other methods
+     /**
+     * Method to check if player is death
+     * 
+     * @param playerPieces
+     */
     public void checkDeath(Iterator<Piece> playerPieces) {
         System.out.println("checked back death");
         while (playerPieces.hasNext()) {
@@ -22,6 +34,12 @@ public class BlackPlayer extends Player {
 
     //overridden methods
     @Override
+    
+     /**
+     * Method that print's out if the black player is still alive or not
+     * 
+     *@return Type: BlackPlayer | Alive: true if the player is alive, else false
+     */
     public String toString(){
         return "Type: BlackPlayer | Alive: " + super.isAlive();
     }
